f05f289e207f6c36e3913d858ca8653b9c53783a
Стив <kenethra.74@gmail.com>
2022-06-18 07:26:39 -0600

    feat: use memoryhandle library

--- bayou/src/main.c
+++ bayou/src/main.c
@@ -1,4 +1,4 @@
-#include "bayou.h"
+#include <memoryhandles/mh.h>
 #include <stdint.h>
 #include <unistd.h>
 
@@ -6,16 +6,16 @@ uint32_t limit_left = '[';
 uint32_t limit_right = ']';
 uint32_t slot = 0x2588; // '█'
 
-static void prev_element(struct bayou *bayou, uint16_t count) {
+static void prev_element(struct mh *bayou, uint16_t count) {
   for (uint16_t i = 0; i < count; ++i) {
-    bayou_element_prev(bayou);
+    mh_element_prev(bayou);
   }
 }
 
-static void init_branch(struct bayou *bayou, uint16_t len, uint16_t start) {
+static void init_branch(struct mh *bayou, uint16_t len, uint16_t start) {
   uint8_t *el;
   for (uint8_t i = 0; i < len; ++i) {
-    el = bayou_add_element(bayou);
+    el = mh_add_element(bayou);
 
     if (el != NULL) {
       *((uint16_t *)el) = i + start;
@@ -23,14 +23,14 @@ static void init_branch(struct bayou *bayou, uint16_t len, uint16_t start) {
   }
 }
 
-static void init_tree(struct bayou *bayou, struct bayou_hole *holes,
-                      struct bayou_branch *branches, void **elements,
+static void init_tree(struct mh *bayou, struct mh_hole *holes,
+                      struct mh_branch *branches, void **elements,
                       uint16_t holes_len, uint16_t branches_len,
                       uint16_t elements_len) {
 
-  bayou_init(bayou, branches, branches_len, 10, branches_len, elements,
-             elements_len, 20, elements_len, sizeof(uint16_t), holes, holes_len,
-             30, holes_len);
+  mh_init(bayou, branches, branches_len, 10, branches_len, elements,
+          elements_len, 20, elements_len, sizeof(uint16_t), holes, holes_len,
+          30, holes_len);
 
   uint16_t start = 0;
   const uint16_t len = 10;
@@ -43,9 +43,9 @@ static void init_tree(struct bayou *bayou, struct bayou_hole *holes,
   init_branch(bayou, len, start);
 
   start += len;
-  bayou_branch_parent(bayou);
-  bayou_branch_child(bayou);
-  bayou_element_set(bayou, count / 2);
+  mh_branch_parent(bayou);
+  mh_branch_child(bayou);
+  mh_element_set(bayou, count / 2);
   init_branch(bayou, len, start);
 }
 
